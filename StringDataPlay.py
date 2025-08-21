1f5f6bf5dd57e42bd556205e56d7809ac3b0d448
anjali21i <anjaliguptajnp21@gmail.com>
2025-08-22 00:49:39 +0530

    String learn updated

--- StringDataPlay.py
+++ StringDataPlay.py
@@ -22,3 +22,48 @@ print(result)
 
 print(f"check extra }} {{")
 
+#string operation
+
+star = "*"
+print(star * 3)
+
+print(star * -1)
+
+print(len("Python"))
+print(ord("A"))
+print("apple">"Apple")
+print(11>2)
+
+print("\u0061")
+print(chr(97))
+
+txtVal = "Python"
+# print(txtVal[len(txtVal)]) #will give: IndexError: string index out of range
+
+print(txtVal[len(txtVal)-1])
+print(txtVal[-2]) #negative indexing
+
+
+#slice => val[start:end:step]
+print(txtVal[0:3])
+print(txtVal[0:])
+print(txtVal[:])
+
+print(txtVal[-5:-2])
+print(txtVal[-2:-5])
+
+print(txtVal[::-2])
+print(txtVal[::-1]) #reverse
+
+
+print(txtVal.upper()) #newString will be returned
+print(txtVal.lower()) #newString will be returned
+print(txtVal.capitalize())#newString will be returned
+print(txtVal.title())
+print(txtVal)
+
+data_with_space= "   Welcome Anjali!   "
+print(data_with_space.strip())
+print(data_with_space.lstrip())
+print(data_with_space.rstrip())
+
